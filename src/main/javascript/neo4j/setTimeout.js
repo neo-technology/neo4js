e7cbcf5aadadc216432ca0075ed0c32d3e806d1e
Jacob Hansson <jakewins@voltvoodoo.com>
2011-02-02 12:46:10 +0100

    Added missing files. Fixed recursion issue with pulse-check method.

---
+++ src/main/javascript/neo4j/setTimeout.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2002-2011 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+_.extend(neo4j, {
+
+    /**
+     * This checks if there is there is a setTimeout method available. If not,
+     * this will trigger the method directly if timeout is 0, or throw
+     * an exception if timeout is greater than that. 
+     */
+    setTimeout : function( expression, timeout ) {
+        if(typeof(setTimeout) != "undefined") {
+            return setTimeout(expression, timeout);
+        } else if (timeout === 0) {
+            expression();
+        } else {
+            neo4j.log("No timeout implementation found, unable to do timed tasks.");
+        }
+    },
+    
+    clearTimeout : function(timeoutId) {
+        if(typeof(clearTimeout) != "undefined") {
+            clearTimeout(intervalId);
+        } else {
+            neo4j.log("No timeout implementation found, unable to do timed tasks.");
+        }
+    },
+    
+    _intervals : {}
+});
---
+++ src/test/javascript/neo4j/WebTestCase.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2002-2011 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var WebTest = function(name) {
+    TestCase.call(this, "WebTest." + name);
+};
+
+WebTest.prototype = new TestCase();
+
+_.extend(WebTest.prototype, {
+    
+    testServerUnresponsive : function() {
+        var result = {eventTriggered:false};
+        clearWebmock();
+        
+        webmock("POST", "http://localhost:7474/db/data/node", function(req) {
+            req.failure(new neo4j.exceptions.ConnectionLostException());
+        });
+        
+        neo4j.events.bind("web.connection.failed", function() {
+            result.eventTriggered = true;
+        });
+        
+        mockWeb.post("http://localhost:7474/db/data/node");
+        
+        this.assertTrue("Connection failed event should have been triggered.", result.eventTriggered);
+        
+    }
+});
